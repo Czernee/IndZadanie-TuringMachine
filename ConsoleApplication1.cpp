b624c13da5f26f5300024a2745fa50e2040e7cb5
Czernee <tehnikvalerij@gmail.com>
2023-04-30 16:03:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+struct len
+    {
+    vector <char> lenta;
+    int gol;
+    };
+
+struct mashin
+    {
+    map <char, char> dvig;
+    map <char, char> zap;
+    map <char, int> smena;
+    };
+
+using namespace std;
+
+int main()
+{
+    
+}
+
